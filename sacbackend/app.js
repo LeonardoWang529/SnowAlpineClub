c413c9ea13437cde5b5c865f7495785df653b1e0
LeonardoWang529 <leo.wangyi57@gmail.com>
2020-04-09 19:16:12 -0700

    Leo back end dev

--- sacbackend/app.js
+++ sacbackend/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postRouter = require('./routes/post');
+var commentRouter = require('./routes/comment');
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +41,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const uri = process.env.ATLAS_URI;
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+});
 module.exports = app;
